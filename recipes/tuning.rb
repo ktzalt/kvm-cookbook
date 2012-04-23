bfd17b95d8673c73afc57c99ed1687dec939da42
Guilhem Lettron <guilhem@lettron.fr>
2012-04-23 19:50:33 +0200

    make tuning working

--- recipes/tuning.rb
+++ recipes/tuning.rb
@@ -1,6 +1,10 @@
 include_recipe "kvm::default"
 
-if node[:cpu][0][:flags].include?("ept")
+include_recipe "sysctl"
+
+include_recipe "modules"
+
+if node[:cpu]["0"][:flags].include?("ept")
   sysctl "vm.swappiness" do
     value "0"
   end

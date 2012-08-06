3f104fe86a04f7dff9ea5e3ad13bb28671239ef8
Guilhem Lettron <guilhem@lettron.fr>
2012-08-06 15:32:38 +0200

    add some tuning (commented for the moment)

--- recipes/tuning.rb
+++ recipes/tuning.rb
@@ -11,3 +11,19 @@ if node[:cpu]["0"][:flags].include?("ept")
 end
 
 modules "vhost_net"
+
+
+include_recipe "sysfs"
+#sysfs "Tuning ondemand cpufreq governor" do
+#  name "devices/system/cpu/cpufreq/ondemand/sampling_down_factor"
+#  value "100"
+#end
+
+# Correct problem with dhcpd and vhost_net
+# http://docs.redhat.com/docs/en-US/Red_Hat_Enterprise_Linux/6/html/Virtualization_Host_Configuration_and_Guest_Installation_Guide/ch11s02.html
+# https://bugs.launchpad.net/ubuntu/+source/isc-dhcp/+bug/930962
+
+# suppress because iptables_rules suppress all others rules
+
+#include_recipe "iptables"
+#iptables_rule "correctUdpDhcp"

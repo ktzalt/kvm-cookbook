b816c19fcf2da7a9adee10b657d828b221a54f56
Guilhem Lettron <guilhem@lettron.fr>
2012-11-13 15:47:23 +0100

    rename README.rdoc to README.md

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "guilhem.lettron@youscribe.com"
 name             "kvm"
 license          "Apache 2.0"
 description      "Installs/Configures/tuning kvm"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 depends          "apt"
 depends          "sysctl"

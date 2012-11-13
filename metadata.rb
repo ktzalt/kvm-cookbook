0309e425cc819ba8d7733df6885d75b7ecf332b7
Guilhem Lettron <guilhem@lettron.fr>
2012-11-13 16:16:26 +0100

    update dependency in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,17 @@
+name             "kvm"
 maintainer       "Guilhem Lettron"
 maintainer_email "guilhem.lettron@youscribe.com"
-name             "kvm"
 license          "Apache 2.0"
 description      "Installs/Configures/tuning kvm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
-depends          "apt"
-depends          "sysctl"
-depends          "modules"
+version          "0.1.1"
+
 depends          "ntp", ">= 1.2.0"
-depends          "sysfs"
-depends          "iptables"
-depends          "cpu"
+
+suggests         "sysctl"
+suggests         "modules"
+suggests         "sysfs"
+suggests         "cpu"
 
 distros = %w(debian ubuntu)
 
